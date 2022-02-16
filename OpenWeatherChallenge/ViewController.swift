87acd5ae190eecfbe8bfc50b79f51b41f5b232de
leonardo.soares.88 <leonardo.soares@mesainc.com.br>
2022-02-16 11:16:37 -0400

    Initial Commit

---
+++ OpenWeatherChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherChallenge
+//
+//  Created by Leonardo Soares on 16/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
